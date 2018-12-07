c787a25d4837ee3cf29e7b62a73232d8da15a184
Jason Gritman <jasongritman@gmail.com>
2018-12-07 16:03:24 -0700

    Initial Commit

---
+++ BeaconTabsTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BeaconTabsTest
+//
+//  Created by Jason Gritman on 12/7/18.
+//  Copyright © 2018 Jason Gritman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
